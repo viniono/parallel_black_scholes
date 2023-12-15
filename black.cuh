9f4fc110de0631c03508f96b5e5f9987f02decef
Tandeta Tartarotti <tandetat@bellman.cs.grinnell.edu>
2023-12-15 14:14:32 -0600

    Remove erf

--- black.cuh
+++ black.cuh
@@ -1,34 +1,8 @@
 #ifndef BLACK_H
 #define BLACK_H
-#include <stdio.h>
 
 //Link for relation of ERF and cdf 
 //https://www.johndcook.com/erf_and_normal_cdf.pdf
-
-// Error Function
-__device__ double erf(double x) {
-	// constants for error function
-//source for optimized constants:
-// https://www.johndcook.com/blog/2009/01/19/stand-alone-error-function-erf/
-	double a1 =  0.254829592;
-	double a2 = -0.284496736;
-	double a3 =  1.421413741;
-	double a4 = -1.453152027;
-	double a5 =  1.061405429;
-	double p  =  0.3275911;
-	int sign = 1;
-	if (x < 0) {
-		sign = -1;
-	}
-	x = fabs(x);
-
-	double t = 1.0 / (1.0 + p*x);
-
-	double y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*exp(-x*x);
-
-	return sign * y;
-}
-
 
 // Normal CDF using error function
 __device__ double cdf(double x) {
@@ -59,6 +33,4 @@ __device__ double BS_PUT(double S, double K, double T, double r, double sigma) {
 	double d2 = D2(d1, sigma, T);
 	return  K * __expf(-r*T) * cdf(-d2) - S * cdf(-d1);
 }
-
-
 #endif
